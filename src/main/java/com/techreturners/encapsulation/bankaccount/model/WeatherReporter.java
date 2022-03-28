1931ebfd37c48b71f06c09eb728092395fc28c0a
Deepana Naidu <deepana.naidu@gmail.com>
2022-03-28 23:26:11 +0100

    Refractored WeatherReporter

--- src/main/java/com/techreturners/encapsulation/bankaccount/model/WeatherReporter.java
+++ src/main/java/com/techreturners/encapsulation/bankaccount/model/WeatherReporter.java
@@ -1,52 +1,56 @@
 package com.techreturners.encapsulation.bankaccount.model;
 
 import java.text.MessageFormat;
+import java.util.HashMap;
 
 public class WeatherReporter {
 
-    public String location;
-    public double temperature;
+    private final String location;
+    private final double temperature;
+    private static final int HOT = 30;
+    private static final int COLD = 10;
+    private static final HashMap<String,String> weatherLocationMap = new HashMap<>();
 
     public WeatherReporter(String location, double temperature) {
         this.location = location;
         this.temperature = temperature;
+        setWeatherLocationMap();
+    }
+
+    private void setWeatherLocationMap(){
+        weatherLocationMap.put("london","🌦");
+        weatherLocationMap.put("california","🌅");
+        weatherLocationMap.put("cape town","🌤");
+        weatherLocationMap.put("others","🔆");
+    }
+
+    public HashMap<String,String> getWeatherLocationMap(){
+        return weatherLocationMap;
     }
 
     public String print() {
-
-        double newTemp = (9.0 / 5.0) * temperature + 32;
-        return MessageFormat.format("I am in {0} and it is {1}. {2}. The temperature in Fahrenheit is {3}.", location, check1(), check2(), newTemp);
-
+        return MessageFormat.format("I am in {0} and it is {1}. {2}. The temperature in Fahrenheit is {3}.", location.substring(0,1).toUpperCase() + location.substring(1).toLowerCase(), weatherBasedOnLocation(location), weatherBasedOnTemperature(temperature), newTemp(temperature));
     }
 
-    public String check1() {
-        if (location == "London") {
-
-            return "🌦";
-
-        } else if (location == "California") {
-
-            return "🌅";
-
-        } else if (location == "Cape Town") {
-
-            return "🌤";
-
-        }
-        return "🔆";
+    private double newTemp(double temperature){
+        return (9.0 / 5.0) * temperature + 32;
     }
 
-    public String check2() {
-        if (temperature > 30) {
+    public String weatherBasedOnLocation(String location) {
+        location = location.toLowerCase();
+        return getWeatherLocationMap().containsKey(location) ? getWeatherLocationMap().get(location) : getWeatherLocationMap().get("others");
+    }
 
-            return "It's too hot 🥵!";
-
-        } else if (temperature < 10) {
-
-            return "It's too cold 🥶!";
-
+    public String weatherBasedOnTemperature(double temperature) {
+        String message;
+        if (temperature > HOT) {
+           message = "It's too hot 🥵!";
+        }else if (temperature < COLD) {
+            message = "It's too cold 🥶!";
+        }else {
+            message = "Ahhh...it's just right 😊!";
         }
-        return "Ahhh...it's just right 😊!";
+        return message;
     }
 
 }
